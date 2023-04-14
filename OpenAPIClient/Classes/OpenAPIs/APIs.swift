7073cf6957886e9a19c9dc0459f99ae600f356a7
Nikola Derikonjic <nikola@pregmatch.org>
2023-04-14 03:39:43 +0200

    update

--- OpenAPIClient/Classes/OpenAPIs/APIs.swift
+++ OpenAPIClient/Classes/OpenAPIs/APIs.swift
@@ -14,7 +14,7 @@ import Foundation
 public typealias OpenAPIClient = OpenAPIClientAPI
 
 open class OpenAPIClientAPI {
-    public static var basePath = "https://api.shinyflake.me"
+    public static var basePath = ""
     public static var customHeaders: [String: String] = [:]
     public static var credential: URLCredential?
     public static var requestBuilderFactory: RequestBuilderFactory = URLSessionRequestBuilderFactory()
